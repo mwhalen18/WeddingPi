a7952f4fd4e1565494b893e277a759edd7b61790
mwhalen18 <matthew.whalen18@gmail.com>
2022-11-21 10:19:08 -0600

    added blink to read module

--- read.py
+++ read.py
@@ -2,13 +2,22 @@
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
+from time import sleep
 
 reader = SimpleMFRC522()
+GPIO.setmode(GPIO.BOARD)
+
+LED_PIN = 11
+GPIO.setup(11, GPIO.OUT)
 
 try:
 	print('Waiting for scan')
 	id = reader.read()[0]
 	print("The ID for the card is: ", id)
+	GPIO.output(11, GPIO.HIGH) 
+	sleep(1)
+	GPIO.output(11, GPIO.LOW)
+	sleep(1)
 
 finally:
 	GPIO.cleanup()
