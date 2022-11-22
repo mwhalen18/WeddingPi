b582a4e4862d42cab2a4afea2cf2d4dfc8ce2132
mwhalen18 <matthew.whalen18@gmail.com>
2022-11-21 20:58:27 -0600

    big ol song dump

--- spotifyTest.py
+++ spotifyTest.py
@@ -31,7 +31,43 @@ cards = {
     926020207699: 'spotify:track:5nNmj1cLH3r4aA4XDJ2bgY',
     361550775397: 'spotify:track:2Lv9mFjcIFPn8zyWF89EAe',
     183913546982: 'spotify:track:5619Ojc6t9evEEs3B7Drhe',
-    439162176569: 'spotify:track:2ZBNclC5wm4GtiWaeh0DMx'
+    439162176569: 'spotify:track:2ZBNclC5wm4GtiWaeh0DMx',
+    356953818192: 'spotify:track:4RQgqR09VmHn345vRhKQ6T',
+    270903477299: 'spotify:track:5MMZJtHOiH1RmQSSe3DJdg',
+    919142799840: 'spotify:track:1PFBQ9GzRhX1txT7eHWGf2',
+    510012294359: 'spotify:track:5pMmWfuL0FTGshYt7HVJ8P',
+    126587181394: 'spotify:track:1q8E1FfFuhd12c5JcJwPxQ',
+    56366407614: 'spotify:track:7vUzRjokJu0HTFASRGz6yG',
+    1005342885094: 'spotify:track:5ubvP9oKmxLUVq506fgLhk',
+    438635143144: 'spotify:track:3SdTKo2uVsxFblQjpScoHy',
+    899881305163: 'spotify:track:78J9MBkAoqfvyeEpQKJDzD',
+    811716969564: 'spotify:track:1xkLLbpo3y4KNy64EFVB9C',
+    449916306615: 'spotify:track:0ctgmKdlPY8xx41wY339Da',
+    353952078098: 'spotify:track:0zGLlXbHlrAyBN1x6sY0rb',
+    316973647002: 'spotify:track:3pf96IFggfQuT6Gafqx2rt',
+    590693991454: 'spotify:track:0iYebKFUSfF72fUu2OW6ZT',
+    268705662129: 'spotify:track:0wz1LjDb9ZNEYwOmDJ3Q4b',
+    755546850395: 'spotify:track:6l7tK5SsMlN8a9ccgeIkpS',
+    1027589473457: 'spotify:track:6cUCckpdlqHJ5Ascf2uH2A',
+    650672538871: 'spotify:track:2WfaOiMkCvy7F5fcp2zZ8L',
+    210455168046: 'spotify:track:61LtVmmkGr8P9I2tSPvdpf',
+    323718153227: 'spotify:track:1qiQduM84A0VeH8Y2uAbqi',
+    1060422419470: 'spotify:track:0K2WjMLZYr09LKwurGRYRE',
+    547308110862: 'spotify:track:0HPD5WQqrq7wPWR7P7Dw1i',
+    601582404738: 'spotify:track:1Mb8WgET5ozEtG0zlibyUy',
+    471291861472: 'spotify:track:6MFQeWtk7kxWGydnJB2y36',
+    8626510135: 'spotify:track:443sErTxKXGiISwjsEBA9u',
+    321286642096: 'spotify:track:6Qm9MaditCcx5V62rVHYqu',
+    286673994979: 'spotify:track:4WFeJTXNHIS2wURtwlAkhu',
+    494090781798: 'spotify:track:013AWvizllIUEC2FOBzOnh',
+    630254601266: 'spotify:track:5ZBeML7Lf3FMEVviTyvi8l',
+    852680056815: 'spotify:track:2aQxJc9qixdnYwPpWuyF8Y',
+    314188694641: 'spotify:track:1B75hgRqe7A4fwee3g3Wmu',
+    717732387213: 'spotify:track:65jrjEhWfAvysKfnojk1i0',
+    
+
+
+    419251758720: 'exit'
 }
 
 # Spotify Authentication
